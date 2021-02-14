d6200e9d2bc4bfdb3afa486701927456a009d74c
A <A@LAPTOP-U6D9P85D>
2021-02-14 15:03:26 +0530

    WH-1220 New Modification

--- src/main/java/com/git/SpringBootTestApplication.java
+++ src/main/java/com/git/SpringBootTestApplication.java
@@ -1,13 +1,19 @@
 package com.git;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootTestApplication {
-
+	private static Logger logger=LoggerFactory.getLogger(SpringBootTestApplication.class);
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringBootTestApplication.class, args);
+		logger.info("New Modification");
+		logger.error("why this error");
+		logger.warn("Already defined object");
 		System.out.println("This is for git to test the code");
 	}
 
